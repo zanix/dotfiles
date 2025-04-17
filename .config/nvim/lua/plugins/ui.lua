fbddd62e53ba79a3408a38e0550b4017319707cc
Joshua Clark <zanix@users.noreply.github.com>
2025-04-16 21:40:03 -0600

    nvim - snacks.nvim: Use modified advanced config - Enable render-markdown

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -65,7 +65,31 @@ return {
 	{
 		"snacks.nvim",
 		opts = {
-			scroll = { enabled = false },
+			-- scroll = { enabled = false },
+			dashboard = {
+				-- example = "advanced",
+				sections = {
+					{ section = "header" },
+					{ section = "keys", gap = 1, padding = 1 },
+					{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+					{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+					{
+						pane = 2,
+						icon = " ",
+						title = "Git Status",
+						section = "terminal",
+						enabled = function()
+							return Snacks.git.get_root() ~= nil
+						end,
+						cmd = "git status --short --branch --renames",
+						height = 5,
+						padding = 1,
+						ttl = 5 * 60,
+						indent = 3,
+					},
+					{ section = "startup" },
+				},
+			},
 		},
 		keys = {},
 	},
@@ -132,11 +156,6 @@ return {
 				}),
 			}
 		end,
-	},
-
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		enabled = false,
 	},
 
 	{
